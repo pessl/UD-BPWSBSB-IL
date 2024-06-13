caf3223463f7b2b8205a315defdba0a0a86c651d
Pedro Soria <psorialopez84@gmail.com>
2024-06-13 00:59:58 -0500

    Second Tasklet Step

--- src/main/java/com/udemy/infybuzz/config/SampleJob.java
+++ src/main/java/com/udemy/infybuzz/config/SampleJob.java
@@ -25,9 +25,11 @@ public class SampleJob {
 	public Job firstJob() {
 		return jobBuilderFactory.get("First Job")
 		.start(firstStep())
+		.next(secondStep())
 		.build();
 	}
 	
+	//First Step
 	private Step firstStep() {
 		return stepBuilderFactory.get("First Step")
 				.tasklet(firstTask())
@@ -40,6 +42,25 @@ public class SampleJob {
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				System.out.println("This is first tasklet step");
+				return RepeatStatus.FINISHED;
+			}
+		};
+	}
+	
+	
+	//Second Step
+	private Step secondStep() {
+		return stepBuilderFactory.get("Second Step")
+				.tasklet(secondTask())
+				.build();
+	}
+	
+	private Tasklet secondTask() {
+		return new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("This is second tasklet step");
 				return RepeatStatus.FINISHED;
 			}
 		};
