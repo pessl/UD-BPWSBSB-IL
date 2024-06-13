0ab894eb239ff3d50f5e9346feed7c19560b67d7
Pedro Soria <psorialopez84@gmail.com>
2024-06-13 00:46:37 -0500

    Create First Spring Batch Application

---
+++ src/test/java/com/udemy/infybuzz/app/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.infybuzz.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
